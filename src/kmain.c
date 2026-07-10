6649e6b41ee1c5920afcf28b9882e86d74c12040
mariagabi-lustosa <lustosa.mariagabriela@gmail.com>
2026-07-10 14:32:02 -0300

    implementing trap handler, timer, serial driver and shell

--- src/kmain.c
+++ src/kmain.c
@@ -3,6 +3,7 @@
 #include <arch/timer.h>
 #include <kernel/trap.h>
 #include <kernel/serial.h>
+#include <kernel/string.h>
 
 extern int _hartid[];
 void kmain()
@@ -21,5 +22,42 @@ void kmain()
 	serial_init();
 	serial_irq_enable();
 
-	/* implement your shell here */
+	hart_irq_enable();
+
+	char line[256];
+	size_t line_len = 0;
+	char buf[256];
+
+	serial_puts("> ");
+
+	while(1) {
+		size_t n = serial_read(buf);
+		for (size_t i = 0; i < n; i++) {
+			char c = buf[i];
+			serial_putc(c);
+
+			if (c ==  '\r') {
+				serial_puts("\n");
+				line[line_len] = '\0';
+
+				if (strcmp(line, "uptime") == 0) {
+					printk(LOG_INFO, "%lus\n", timer_read());
+				} else if (strncmp(line, "echo ", 5) == 0) {
+					serial_puts(line + 5);
+					serial_puts("\n");
+				} else if (strncmp(line, "alarm ", 6) == 0) {
+					u64 secs = strtou64(line + 6, 10);
+					timer_set_alarm(secs);
+					serial_puts("alarm set\n");
+				}
+
+				line_len = 0;
+				serial_puts("> ");
+			} else {
+				if (line_len < sizeof(line) - 1) {
+				line[line_len++] = c;
+				}
+			}
+		}
+	}
 }
